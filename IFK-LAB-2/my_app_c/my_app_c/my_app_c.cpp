ac9e983f64123384216188f4aa12d9fadf925c7f
melancholy314 <55665750+melancholy314@users.noreply.github.com>
2023-05-12 20:48:14 +0300

    Add files via upload

---
+++ IFK-LAB-2/my_app_c/my_app_c/my_app_c.cpp
@@ -0,0 +1,39 @@
+﻿#include<stdio.h>  
+#include<time.h>
+#include<string.h>
+
+int main() {
+	FILE* file;
+	if (fopen_s(&file, "C:/Users/kku/Desktop/test.kku", "r+")) {
+		printf_s("Error on open file!\n");
+		return 0;
+	}
+	//if (fopen_s(&file, "D:/1.txt", "r+")) {
+	//	printf_s("Error on open file!\n");
+	//	return 0;
+	//}
+	char buffer[1024] = { 0 };
+	fread(buffer, sizeof(char), 1024, file);
+	printf_s("% s", buffer);
+	printf_s("\n");
+
+	time_t rawtime;
+	struct tm info;
+	time(&rawtime);
+	localtime_s(&info, &rawtime);
+
+	memset(buffer, '\0', 1024);
+	strcat_s(buffer, "Korotaev_Kirill_Korotaev_Kirill_Korotaev_Kirill_Korotaev_201-331");
+	size_t len = strlen(buffer);
+
+	fseek(file, 0, 0);
+	fwrite(buffer, sizeof(char), len, file);
+
+	fclose(file);
+
+	getchar();
+
+	return 0;
+
+
+}
